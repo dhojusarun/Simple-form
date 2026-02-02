b443ceb79ff069a685f387a9daf8341a938a8720
Sarun Dhoju <sarundhoju775@gmail.com>
2026-02-02 21:34:55 +0545

    changes and input field for header added

--- Movie-app/src/components/Header.tsx
+++ Movie-app/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
+import Search from "./input fields/Search";
 function Header() {
   return (
     <>
       <nav className="navbar">
         <h1 className="logo">Logo</h1>
+        <Search/>
         <ul className="nav-links">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/trendingmovies">Trending Movies</NavLink>
---
+++ Movie-app/src/components/input fields/Search.tsx
@@ -0,0 +1,16 @@
+import { FaSearch } from "react-icons/fa";
+function Search() {
+  return (
+   <div className="search-bar">
+  <FaSearch className="search-icon" />
+  <input
+    type="text"
+    placeholder="Search movies..."
+    className="search-input"
+  />
+</div>
+
+  );
+}
+
+export default Search;
